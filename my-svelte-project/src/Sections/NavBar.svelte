0cbe6594bd25f2b6ffd3f07f720ef47d4b9f3b4b
arvreza <32081264+arvreza@users.noreply.github.com>
2021-02-25 00:33:36 -0800

    first project

---
+++ my-svelte-project/src/Sections/NavBar.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let title;
+</script>
+
+<style>
+    nav {
+        width: 100%;
+        font-size: 2rem;
+        background-color: blueviolet;
+        color: yellow;
+        text-align: center;
+    }
+</style>
+
+<nav>{title}</nav>
