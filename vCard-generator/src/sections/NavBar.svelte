f04c084874c614af8d43cb6767784a924d931f2e
arvreza <32081264+arvreza@users.noreply.github.com>
2021-03-01 00:43:06 -0800

    DOM events

--- vCard-generator/src/sections/NavBar.svelte
+++ vCard-generator/src/sections/NavBar.svelte
@@ -1,5 +1,15 @@
 <script>
     export let title;
+
+    let disabled = true;
+
+    function keyPressed() {
+        disabled = false;
+    }
+
+    function resetPressed() {
+        disabled = true;
+    }
 </script>
 
 <style>
@@ -13,3 +23,14 @@
 </style>
 
 <nav>{title}</nav>
+
+<form>
+    <label>
+        Name 
+        <input type="text" 
+            placeholder="Please input your name:" 
+            on:keydown={ keyPressed } />
+    </label>
+    <button type="reset" {disabled} on:click={resetPressed}>Reset</button>
+</form>
+
